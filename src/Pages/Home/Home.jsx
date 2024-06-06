3f796fd4e9d76a022a85df727a0f0ec415ab9437
BR-Jayanth <jayanthbychana@gmail.com>
2024-06-06 17:45:47 +0530

    bug on homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
-import "./Home.css"
+import styles from "./Home.module.css"
+// import "./Home.css"
 
 import { Link } from 'react-router-dom'
 
 export const Home = () => {
   // home page of the application
   return (
-    <div className="home-container">
+    <div className={styles.homeContainer}>
       
-      <div className="home-content">
+      <div className={styles.homeContent}>
         <h1>
           You got the travel plans, we got the travel vans.
         </h1>
-        <div className="home-content-2">
+        <div className={styles.homeContent2}>
           <p>
             Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.
           </p>
-          <div className='home-link'>
-            <Link to="/vans" className='link-button-home' >Find your van</Link>
+          <div className={styles.homeCink}>
+            <Link to="/vans" className={styles.linkButtonHome} >Find your van</Link>
           </div>
 
         </div>
