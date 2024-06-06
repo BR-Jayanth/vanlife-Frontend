5ebc6d734d7e68561e7cf45727e9393d2e0a6ccc
BR-Jayanth <jayanthbychana@gmail.com>
2024-06-06 17:46:38 +0530

    bug on homepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -17,7 +17,7 @@ export const Home = () => {
           <p>
             Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.
           </p>
-          <div className={styles.homeCink}>
+          <div className={styles.homeLink}>
             <Link to="/vans" className={styles.linkButtonHome} >Find your van</Link>
           </div>
 
